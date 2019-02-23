7f7f28f62f66e526e728846768fb66f05d3b3bb3
Gabriel <gabaofogo@hotmail.com>
2019-02-23 20:43:17 -0300

    Agora vai #2

--- backend_abundancia/settings.py
+++ backend_abundancia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eb9ehno*a(%(=c4r%5vg00b%s_563+8ho57kmk%op@$h0)fg9#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['immense-wave-53840.herokuapp.com']
 
 
 # Application definition
