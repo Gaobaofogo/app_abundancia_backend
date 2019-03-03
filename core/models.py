9c39e0501a3c4094d35c24652a469bf7d5ffb2a0
Gabriel <gabaofogo@hotmail.com>
2019-03-03 16:45:35 -0300

    Adicionada opção de vídeo nas tarefas

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
+    video = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
